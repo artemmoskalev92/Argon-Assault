00b7580400dfaf9e5dee27b3a5adc8690b111bf4
artemmoskalev92 <83223168+artemmoskalev92@users.noreply.github.com>
2021-11-12 20:38:43 +0500

    Доработка

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -7,11 +7,30 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     void OnParticleCollision(GameObject other)
     {
-       GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        ProcessHit();
+        DestroyEnemy();
+    }
+
+    void DestroyEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 }
